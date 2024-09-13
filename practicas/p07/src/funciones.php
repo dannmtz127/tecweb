a2b77d1adaeb6e7fde8c3950dd588d753180ec1b
dannmtz127 <daniel.martineza@alumno.buap.mx>
2024-09-12 22:35:45 -0600

    archivo funciones.php creado

---
+++ practicas/p07/src/funciones.php
@@ -0,0 +1,9 @@
+<?php
+function MultiploDe5y7($numero) {
+    if ($numero % 5 == 0 && $numero % 7 == 0) {
+        return "El número $numero SÍ es múltiplo de 5 y 7.";
+    } else {
+        return "El número $numero NO es múltiplo de 5 y 7.";
+    }
+}
+?>
