69a33931ae6e3d46eee47620541398a0aa1ff3b4
dannmtz127 <daniel.martineza@alumno.buap.mx>
2024-11-23 21:17:16 -0600

    read.php completado

--- practicas/p11/product_app/backend/read.php
+++ practicas/p11/product_app/backend/read.php
@@ -1,34 +1,60 @@
 <?php
-include_once __DIR__.'/database.php';
+    include_once __DIR__.'/database.php';
 
-// SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
-$data = array();
-// SE VERIFICA HABER RECIBIDO EL ID
-if (isset($_POST['search'])) {
-    $search = $_POST['search'];
+    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+    $data = array();
+    // SE VERIFICA HABER RECIBIDO EL ID
+    if( isset($_POST['id']) ) {
+        $id = $_POST['id'];
+        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+        if ( $result = $conexion->query("SELECT * FROM productos WHERE id = '{$id}'") ) {
+            // SE OBTIENEN LOS RESULTADOS
+			$row = $result->fetch_array(MYSQLI_ASSOC);
 
-    // SE REALIZA LA QUERY DE BÚSQUEDA
-    $query = "SELECT * FROM productos WHERE nombre LIKE '%{$search}%' 
-              OR marca LIKE '%{$search}%' 
-              OR detalles LIKE '%{$search}%'";
-    
-    if ($result = $conexion->query($query)) {
-        // SE OBTIENEN LOS RESULTADOS
-        while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-            $data[] = array(
-                'id' => utf8_encode($row['id']),
-                'nombre' => utf8_encode($row['nombre']),
-                'marca' => utf8_encode($row['marca']),
-                'detalles' => utf8_encode($row['detalles'])
-            );
+            if(!is_null($row)) {
+                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
+                foreach($row as $key => $value) {
+                    $data[$key] = utf8_encode($value);
+                }
+            }
+			$result->free();
+		} else {
+            die('Query Error: '.mysqli_error($conexion));
         }
-        $result->free();
-    } else {
-        die('Query Error: ' . mysqli_error($conexion));
-    }
-    $conexion->close();
-}
-
-// SE HACE LA CONVERSIÓN DE ARRAY A JSON
-echo json_encode($data, JSON_PRETTY_PRINT);
+		$conexion->close();
+    } 
+    elseif( isset($_POST['carac']) ) {
+        $carac = $_POST['carac'];
+        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+        #$result = $conexion->query("SELECT id, nombre FROM productos WHERE nombre = '{$id}' LIKE ? LIMIT 0, 5")
+        $consulta = "SELECT DISTINCT id, nombre FROM productos 
+                        WHERE nombre LIKE '%{$carac}%'
+                        OR detalles LIKE '%{$carac}%'
+                        OR marca LIKE '%{$carac}%'
+                        LIMIT 0, 5";    
+        if ($result = $conexion->query($consulta)){
+            // SE OBTIENEN LOS RESULTADOS
+			//$row = $result->fetch_array(MYSQLI_ASSOC);
+            $row = $result->fetch_all(MYSQLI_ASSOC);
+            #echo $row;
+            if(!is_null($row)) {
+                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
+                /*foreach($row as $key => $value) {
+                    $data[$key] = utf8_encode($value);
+                }*/
+                foreach($row as $num => $registro) {            // Se recorren tuplas
+                    foreach($registro as $key => $value) {      // Se recorren campos
+                        $data[$num][$key] = ($value);
+                    }
+                }
+            }
+			$result->free();
+		} else {
+            die('Query Error: '.mysqli_error($conexion));
+        }
+		$conexion->close();
+    } 
+    
+    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
+    echo json_encode($data, JSON_PRETTY_PRINT);
 ?>
