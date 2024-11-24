d1e3e172429d9145200439404b23f0362b09fd14
dannmtz127 <daniel.martineza@alumno.buap.mx>
2024-11-23 21:14:40 -0600

    database.php completado

--- practicas/p11/product_app/backend/database.php
+++ practicas/p11/product_app/backend/database.php
@@ -1,16 +1,15 @@
 <?php
-    // Crear conexión a la base de datos
     $conexion = @mysqli_connect(
-        'localhost',      // Servidor
-        'root',          // Usuario
-        '123456789a', // Contraseña
-        'marketzone'     // Nombre de la base de datos
+        'localhost',
+        'root',
+        '123456789a',
+        'marketzone'
     );
 
     /**
-     * NOTA: si la conexión falló, $conexion contendrá false.
+     * NOTA: si la conexión falló $conexion contendrá false
      **/
-    if (!$conexion) {
-        die('¡Base de datos NO conectada! ' . mysqli_connect_error());
+    if(!$conexion) {
+        die('¡Base de datos NO conectada!');
     }
 ?>
